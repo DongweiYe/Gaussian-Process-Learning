1b09e2a5c4add7291e8db923e5b0a5f4ffa635f9
Dongwei Ye <ye_dongwei@hotmail.com>
2023-07-13 12:08:50 +0200

    prediction visualization ready

--- Lotka-Volterra-model/Scene1.py
+++ Lotka-Volterra-model/Scene1.py
@@ -12,35 +12,39 @@ np.random.seed(0)
 ### This applies to Kdd, Kuu, Kud and Kdu
 
 ### Parameters
-TrainRatio = 0.4
-DataSparsity = 0.005
-NoiseMean = 0
-NoiseSTD = 0.2 ### STD
-NumDyn = 2
-PosteriorSample = 200
+TrainRatio = 0.4         ### Trian/Test data split ratio
+DataSparsity = 0.005      ### Take 25% of as the total data we have
+NoiseMean = 0            ### 0 mean for white noise
+NoisePer = 0.1           ### (0 to 1) percentage of noise. NoisePer*average of data = STD of white noise
+NumDyn = 2               ### number of dynamics equation
+PosteriorSample = 1000    ### posterior sampling numbers
 
 ### Load data and add noise
 x1 = np.load('data/x1.npy')
 x2 = np.load('data/x2.npy')
 timedata = np.load('data/time.npy')
 
-preydata = x1 + np.random.normal(NoiseMean,NoiseSTD,x1.shape[0])
-preddata = x2 + np.random.normal(NoiseMean,NoiseSTD,x2.shape[0])
+
+NoiseSTD1 = NoisePer*np.mean(x1)
+NoiseSTD2 = NoisePer*np.mean(x2)
+
+preydata = x1 + np.random.normal(NoiseMean,NoiseSTD1,x1.shape[0])
+preddata = x2 + np.random.normal(NoiseMean,NoiseSTD2,x2.shape[0])
 
 num_data = preddata.shape[0] - 1 ### 0 -> K, using [0,K-1] for int
 num_train = int((num_data*TrainRatio)*DataSparsity) 
 samplelist = np.random.choice(np.arange(0,int(num_data*TrainRatio)),num_train,replace=False)
-# samplelist = np.arange(0,8000,100)
+
 
 ### Define training data 
 Xtrain = np.expand_dims(timedata[samplelist],axis=1)
 ytrain = np.hstack((np.expand_dims(preydata[samplelist],axis=1),np.expand_dims(preddata[samplelist],axis=1)))
 ytrain_backup = np.hstack((np.expand_dims(x1[samplelist],axis=1),np.expand_dims(x2[samplelist],axis=1)))
 
-plt.plot(Xtrain,ytrain[:,0],'*',label='x1 dynamics')
-plt.plot(Xtrain,ytrain[:,1],'*',label='x2 dynamics')
-plt.legend()
-plt.show()
+# plt.plot(Xtrain,ytrain[:,0],'*',label='x1 dynamics')
+# plt.plot(Xtrain,ytrain[:,1],'*',label='x2 dynamics')
+# plt.legend()
+# plt.show()
 
 ### Build a GP to infer the hyperparameters for each dynamic equation 
 ### and proper G_i data from regression
@@ -79,8 +83,13 @@ for i in range(0,NumDyn):
     print('GP hyperparameters:',GPvariance,GPlengthscale,GPnoise)    
 
     ### Construct the covariance matrix of equation (5)
-    Kuu = kernellist[i].K(Xtrain) + np.identity(Xtrain.shape[0])*GPnoise                 ### invertable
-    Kdd = kernellist[i].dK2_dXdX2(Xtrain,Xtrain,0,0) + np.identity(Xtrain.shape[0])*GPnoise ### Additional noise to make sure invertable
+    if NoisePer == 0:
+        Kuu = kernellist[i].K(Xtrain) + np.identity(Xtrain.shape[0])*1e-4
+        Kdd = kernellist[i].dK2_dXdX2(Xtrain,Xtrain,0,0) + np.identity(Xtrain.shape[0])*1e-4
+    else:
+        Kuu = kernellist[i].K(Xtrain) + np.identity(Xtrain.shape[0])*GPnoise                 ### invertable
+        Kdd = kernellist[i].dK2_dXdX2(Xtrain,Xtrain,0,0) + np.identity(Xtrain.shape[0])*GPnoise ### Additional noise to make sure invertable
+
     Kdu = kernellist[i].dK_dX(Xtrain,Xtrain,0)                                            ### not invertable
     Kud = Kdu.T                                                                      ### not invertable
     invKuu = np.linalg.inv(Kuu)                                                  
@@ -122,7 +131,7 @@ for i in range(PosteriorSample):
     T = 20
 
     preylist,predatorlist = LVmodel(x1_t0,x2_t0,T,dt,[mu1[0],-mu1[1],mu2[0],-mu2[1]])
-    if np.max(preylist) > 2000:
+    if np.max(preylist) > 20 or np.max(predatorlist) > 20:
         pass
     else:
         preylist_array.append(preylist)
@@ -136,19 +145,38 @@ predstd = np.std(np.asarray(predlist_array),axis=0)
 
 
 plt.figure(figsize=(17, 2))
-plt.plot(timedata,x1,'-k',linewidth=3,label='groundtruth')
+params = {
+            'axes.labelsize': 21,
+            'font.size': 21,
+            'legend.fontsize': 23,
+            'xtick.labelsize': 21,
+            'ytick.labelsize': 21,
+            'text.usetex': False,
+            'axes.linewidth': 2,
+            'xtick.major.width': 2,
+            'ytick.major.width': 2,
+            'xtick.major.size': 2,
+            'ytick.major.size': 2,
+        }
+plt.rcParams.update(params)
+
+
 plt.plot(timedata,x2,'-k',linewidth=3)
 
-plt.plot(timedata,preymean,'--',color='royalblue',linewidth=3,label='prey prediction')
-plt.plot(timedata,predmean,'--',color='tab:orange',linewidth=3,label='predator prediction')
+plt.plot(timedata,preymean,'--',color='royalblue',linewidth=3,label=r'$x_1$ prediction')
+plt.plot(timedata,predmean,'--',color='tab:orange',linewidth=3,label=r'$x_2$ prediction')
 plt.fill_between(timedata,preymean+preystd,preymean-preystd,color='royalblue',alpha=0.5)
 plt.fill_between(timedata,predmean+predstd,predmean-predstd,color='tab:orange',alpha=0.5)
 
-plt.scatter(Xtrain,ytrain[:,0],marker='X',s=80,color='royalblue',edgecolors='k',label='training data (prey)',zorder=2)
-plt.scatter(Xtrain,ytrain[:,1],marker='X',s=80,color='darkorange',edgecolors='k',label='training data (predator)',zorder=2)
+plt.scatter(Xtrain,ytrain[:,0],marker='X',s=80,color='royalblue',edgecolors='k',label='training data '+r'($x_1$)',zorder=2)
+plt.scatter(Xtrain,ytrain[:,1],marker='X',s=80,color='darkorange',edgecolors='k',label='training data '+r'($x_2$)',zorder=2)
 
 plt.axvline(timedata[-1]*TrainRatio,linestyle='-',linewidth=3,color='grey')
+plt.plot(timedata,x1,'-k',linewidth=3,label='ground truth')
 
-# plt.legend()
+if NoisePer == 0:
+    plt.ylim([-0.8,8])
+# plt.xlim([-1,20])
+plt.legend(loc='upper left',bbox_to_anchor=(0.0, -0.5),ncols=3,frameon=False)
 # plt.show()
-plt.savefig('N'+str(int(NoiseSTD*1000))+'D'+str(int(DataSparsity*1000))+'.png',bbox_inches='tight')
+plt.savefig('result/figure/N'+str(int(NoisePer*100))+'D'+str(int(DataSparsity*400))+'.png',bbox_inches='tight')
