db30852a0fd1c5f6744f7ae908827839230f783b
Dongwei Ye <ye_dongwei@hotmail.com>
2023-12-13 23:36:03 +0100

    update initial contion codes

--- Example3-NN-approximation/Scene3_IC.py
+++ Example3-NN-approximation/Scene3_IC.py
@@ -16,76 +16,114 @@ np.random.seed(0)
 
 ### Parameters
 TrainRatio = 0.25         ### Train/Test data split ratio
-DataSparsity = 0.025       ### Similar to previous examples, we take 25% of data as total data, i.e. DataSparsity = 0.25 (100% data)
+DataSparsity = 0.01       ### Similar to previous examples, we take 25% of data as total data, i.e. DataSparsity = 0.25 (100% data)
 NoiseMean = 0            ### 0 mean for white noise
 NoisePer = 0         ### (0 to 1) percentage of noise. NoisePer*average of data = STD of white noise
 ShapeNN = [1,8,8,1]        ### nodes of each layer of a neural network, e.g. [1,8,8,1]
 PosteriorSample = 1000    ### number of posterior samples from MCMC
+num_ic = 3
 
 ### Load data and add noise
-x = np.load('data/ODEdata.npy')
+x_list = []
+for i in range(num_ic):
+    x_list.append(np.load('data/ODEdata_'+str(i)+'.npy'))
 timedata = np.load('data/time.npy')
 
-NoiseSTD = NoisePer*np.mean(x)
-xdata = x + np.random.normal(NoiseMean,NoiseSTD,x.shape[0])
-
-### Compute the required training data and randomly sample from the list
-num_data = x.shape[0] - 1  ### 0 -> K, using [0,K-1] for int
+num_data = timedata.shape[0] - 1  ### 0 -> K, using [0,K-1] for int
 num_train = int((num_data*TrainRatio)*DataSparsity) 
+
 samplelist = np.random.choice(np.arange(0,int(num_data*TrainRatio)),num_train,replace=False)
-
-print('Data from training: ',num_train)
-
-### Define training data 
 Xtrain = np.expand_dims(timedata[samplelist],axis=1)
-ytrain = np.expand_dims(xdata[samplelist],axis=1)
+
+NoiseSTD = NoisePer*np.mean(x_list[0])
+
+xdata_list = []
+ytrain_list = []
+
+for i in range(num_ic):
+    xdata_list.append(x_list[i] + np.random.normal(NoiseMean,NoiseSTD,x_list[i].shape[0]))
+    ytrain_list.append(np.expand_dims(xdata_list[i][samplelist],axis=1))
+
+    plt.plot(Xtrain,ytrain_list[i],'*',label='x data')
+    plt.plot(timedata,x_list[i],'-k',label='x')
+plt.legend()
+plt.savefig('data.png')
+
+print('Data from training: ',num_train*num_ic)
 
 
 ### Build a GP to infer the hyperparameters for each dynamic equation 
 ### and compute d_i conditioning on u_i
-xtkernel = GPy.kern.RBF(input_dim=1, variance=1, lengthscale=2)
-xtGP = GPy.models.GPRegression(Xtrain,ytrain,xtkernel)
 
-xtGP.optimize(messages=False, max_f_eval=1, max_iters=1e7)
-xtGP.optimize_restarts(num_restarts=2,verbose=False)
+ytrain_hat_list = []
+kernel_list = []
+GP_list = []
+plt.clf()
+for i in range(num_ic):
+    xtkernel = GPy.kern.RBF(input_dim=1, variance=1, lengthscale=2)
+    xtGP = GPy.models.GPRegression(Xtrain,ytrain_list[i],xtkernel)
 
-ytrain_hat,ytrain_hat_var = xtGP.predict(Xtrain)
+    xtGP.optimize(messages=False, max_f_eval=1, max_iters=1e7)
+    xtGP.optimize_restarts(num_restarts=2,verbose=False)
 
-### Vsiualization
-plt.plot(Xtrain,ytrain,'b*',label='GT')
-plt.plot(Xtrain,ytrain_hat,'o',color='tab:red',label='GP prediction',alpha=0.5)
+    ytrain_hat,ytrain_hat_var = xtGP.predict(Xtrain)
+    ytrain_hat_list.append(ytrain_hat)
+    kernel_list.append(xtkernel)
+    GP_list.append(xtGP)
+    ### Vsiualization
+    plt.plot(Xtrain,ytrain_list[i],'b*',label='GT')
+    plt.plot(Xtrain,ytrain_hat,'o',color='tab:red',label='GP prediction',alpha=0.5)
+
 plt.legend()
-plt.savefig('GP_and_data.png')
+plt.savefig('GPprediction.png')
 
 ### Print out hyperparameters from a GP of x(t)
-GPvariance = xtkernel[0]
-GPlengthscale = xtkernel[1]
-GPnoise = xtGP['Gaussian_noise'][0][0]
-print('GP hyperparameters:',GPvariance,GPlengthscale,GPnoise)    
+Kuu_list = []
+Kdd_list = []
+Kdu_list = []
+Kud_list = []
+Rdd_list = []
+Rdu_list = []
+invKuu_list = []
+invRdd_list = []
+d_hat_list = []
 
-### Construct the covariance matrix 
-if NoisePer == 0:
-    Kuu = xtkernel.K(Xtrain) + np.identity(Xtrain.shape[0])*1e-8
-    Kdd = xtkernel.dK2_dXdX2(Xtrain,Xtrain,0,0) + np.identity(Xtrain.shape[0])*1e-8
-else:
-    Kuu = xtkernel.K(Xtrain) + np.identity(Xtrain.shape[0])*GPnoise                    
-    Kdd = xtkernel.dK2_dXdX2(Xtrain,Xtrain,0,0) + np.identity(Xtrain.shape[0])*GPnoise
+for i in range(num_ic):
+    print('     Prepare IC data: ',i)
 
-Kdu = xtkernel.dK_dX(Xtrain,Xtrain,0)
-Kud = Kdu.T
-invKuu = np.linalg.inv(Kuu)                                                  
+    GPvariance = kernel_list[i][0]
+    GPlengthscale = kernel_list[i][1]
+    GPnoise = GP_list[i]['Gaussian_noise'][0][0]
+    print('     GP hyperparameters:',GPvariance,GPlengthscale,GPnoise)  
 
-invRdd = Kdd-Kdu@invKuu@Kud
-Rdd = np.linalg.inv(invRdd)
+    ### Construct the covariance matrix 
+    if NoisePer == 0:
+        Kuu_list.append(kernel_list[i].K(Xtrain) + np.identity(Xtrain.shape[0])*1e-8)
+        Kdd_list.append(kernel_list[i].dK2_dXdX2(Xtrain,Xtrain,0,0) + np.identity(Xtrain.shape[0])*1e-8)
+    else:
+        Kuu_list.append(kernel_list[i].K(Xtrain) + np.identity(Xtrain.shape[0])*GPnoise)                    
+        Kdd_list.append(kernel_list[i].dK2_dXdX2(Xtrain,Xtrain,0,0) + np.identity(Xtrain.shape[0])*GPnoise)
 
-### Compute the true value of d_i using GP
-d_hat = Kdu@invKuu@ytrain
+    Kdu_list.append(kernel_list[i].dK_dX(Xtrain,Xtrain,0))
+    Kud_list.append(Kdu_list[i].T)
+    invKuu_list.append(np.linalg.inv(Kuu_list[i]))
 
+    invRdd_list.append(Kdd_list[i]-Kdu_list[i]@invKuu_list[i]@Kud_list[i])
+    Rdd_list.append(np.linalg.inv(invRdd_list[i]))
 
+    ### Compute the true value of d_i using GP
+    d_hat_list.append(Kdu_list[i]@invKuu_list[i]@ytrain_list[i])
+
+# print(d_hat_list)
+# print(ytrain_hat_list)
 ##############################################
 ############# Build neural network ###########
 ##############################################
 
+Y_obs_list = []
+layer1_list = []
+layer2_list = []
+out_list = []
 ### Construct structure of NN to the dynamics systems
 with pm.Model() as model:
 
@@ -97,13 +135,13 @@ with pm.Model() as model:
     layer3_weight = pm.Normal('l3_w', 0, sigma=10, shape=(ShapeNN[2],ShapeNN[3]))
 
     ### Feedforward
-    layer1 = pm.math.tanh(pm.math.dot(ytrain_hat,layer1_weight) + layer1_bias)
-    layer2 = pm.math.tanh(pm.math.dot(layer1,layer2_weight) + layer2_bias)
-    out = pm.math.dot(layer2,layer3_weight)
+    for i in range(num_ic):
+        layer1_list.append(pm.math.tanh(pm.math.dot(ytrain_hat_list[i],layer1_weight) + layer1_bias))
+        layer2_list.append(pm.math.tanh(pm.math.dot(layer1_list[i],layer2_weight) + layer2_bias))
+        out_list.append(pm.math.dot(layer2_list[i],layer3_weight))
+        Y_obs_list.append(pm.MvNormal('Y_obs_'+str(i), mu=out_list[i], cov=invRdd_list[0], observed=d_hat_list[i]))
     # layer1 = pm.Deterministic('a1',pm.math.tanh(layer1_weight*ytrain_hat+layer1_bias))
     # layer2 = pm.Deterministic('a2',layer2_weight*layer1)
-
-    Y_obs = pm.MvNormal('Y_obs', mu=out, cov=invRdd, observed=d_hat)
     
     step = pm.Metropolis()
     trace = pm.sample(PosteriorSample,step=step, return_inferencedata=False,cores=4,tune=1000,random_seed=0)
@@ -123,11 +161,11 @@ posterior_samples_l3w = np.squeeze(trace.get_values("l3_w", combine=True))
 
 
 ### Plot fi (prediction by NN)
-print('Max x for training: ',np.max(ytrain_hat))
-print('Min x for training: ',np.min(ytrain_hat))
-expscale = 1*(np.max(ytrain_hat) - np.min(ytrain_hat))
+print('Max x for training: ',np.max(x_list[0]))
+print('Min x for training: ',np.min(x_list[0]))
+# expscale = 1*(np.max(ytrain_hat) - np.min(ytrain_hat))
 # fi_input = np.expand_dims(np.arange(0,0.5,0.01),axis=1)
-fi_input = np.expand_dims(np.arange(0,np.max(ytrain_hat)+expscale,0.01),axis=1)
+fi_input = np.expand_dims(np.arange(np.min(x_list[0]),np.max(x_list[0]),0.01),axis=1)
 
 f_predlist = []
 for i in range(PosteriorSample*4):
@@ -165,7 +203,8 @@ if legend_switch == 0:
     plt.plot(fi_input,-np.square(fi_input)+fi_input,'-k',linewidth=3,label='ground truth')
 plt.plot(fi_input,f_prediction_mean,'--',color='crimson',linewidth=3,label=r'NN prediction')
 plt.fill_between(np.squeeze(fi_input),f_prediction_mean+f_prediction_std,f_prediction_mean-f_prediction_std,color='crimson',alpha=0.4,label='NN uncertainty')
-plt.scatter(ytrain_hat,d_hat,marker='X',s=80,color='crimson',edgecolors='k',label='training data',zorder=2)
+for i in range(num_ic):
+    plt.scatter(ytrain_hat_list[i],d_hat_list[i],marker='X',s=80,color='crimson',edgecolors='k',label='training data',zorder=2)
 if legend_switch == 1:
     plt.plot(fi_input,-np.square(fi_input)+fi_input,'-k',linewidth=3,label='ground truth')
     plt.legend(loc='upper left',bbox_to_anchor=(0.0, -0.5),ncol=2,frameon=False)
@@ -223,12 +262,12 @@ params = {
 plt.rcParams.update(params)
 
 if legend_switch == 0:
-    plt.plot(timedata,x,'-k',linewidth=3,label='ground truth')
+    plt.plot(timedata,x_list[0],'-k',linewidth=3,label='ground truth')
 plt.plot(timedata,prediction_mean,'--',color='tab:blue',linewidth=3,label=r'$x$ prediction')
 plt.fill_between(timedata,prediction_mean+prediction_std,prediction_mean-prediction_std,color='tab:blue',alpha=0.4,label=r'$x$ uncertainty')
-plt.scatter(Xtrain,ytrain,marker='X',s=80,color='tab:blue',edgecolors='k',label='training data',zorder=2)
+plt.scatter(Xtrain,ytrain_list[0],marker='X',s=80,color='tab:blue',edgecolors='k',label='training data',zorder=2)
 if legend_switch == 1:
-    plt.plot(timedata,x,'-k',linewidth=3,label='ground truth')
+    plt.plot(timedata,x_list[0],'-k',linewidth=3,label='ground truth')
     plt.legend(loc='upper left',bbox_to_anchor=(0.0, -0.5),ncol=2,frameon=False)
 
 plt.axvline(timedata[-1]*TrainRatio,linestyle='-',linewidth=3,color='grey')
--- Example3-NN-approximation/data_generation.py
+++ Example3-NN-approximation/data_generation.py
@@ -8,24 +8,26 @@ from dynamical_system import *
 ################## for senario 3 (neurl ODE)
 
 ### Definite parameters
-x_t0 = 0.1 ### initial condition
-
 alpha = -1
 beta = 1
-
-
 dt = 1e-3
 T = 5
 
 time = np.arange(0,T+(T/(T/dt))*0.1,T/(T/dt))
 
-### Example 
+x_t0 = 0.1 ### initial condition
 xlist = ODEmodel(x_t0,T,dt,[alpha,beta])
+np.save('data/ODEdata_0.npy',xlist)
+np.save('data/time.npy',time)
 
+
+x_t0 = 0.4 ### initial condition
+xlist = ODEmodel(x_t0,T,dt,[alpha,beta])
+np.save('data/ODEdata_1.npy',xlist)
+
+x_t0 = 0.75 ### initial condition
+xlist = ODEmodel(x_t0,T,dt,[alpha,beta])
+np.save('data/ODEdata_2.npy',xlist)
 
 plt.plot(time,xlist)
 plt.savefig('data.png',bbox_inches='tight')
-
-
-np.save('data/ODEdata.npy',xlist)
-np.save('data/time.npy',time)
